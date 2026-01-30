d6ed9e89650c347bfc7eb089542891ccf778a4e7
JotaErrE01 <jruiz967@gmail.com>
2026-01-30 16:20:12 -0500

    feat: cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [tailwind(), react()],
+  adapter: cloudflare(),
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
